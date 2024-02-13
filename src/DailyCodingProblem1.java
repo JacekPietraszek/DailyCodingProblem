062cf118617816e040bef64c75a2bf8367c9057f
Jacek Pietraszek <jacekpietraszek01@gmail.com>
2024-02-13 17:47:10 +0100

    adds break when sum = k number

--- src/DailyCodingProblem1.java
+++ src/DailyCodingProblem1.java
@@ -5,16 +5,17 @@ public class DailyCodingProblem1 {
 
         int k = 17;
         int y = 0;
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        ArrayList<Integer> numbers = new ArrayList<>();
 
         numbers.add(10);
-        numbers.add(5);
+        numbers.add(7);
         numbers.add(2);
         numbers.add(7);
 
         for (int x = 1; x <= 3; x++) {
             if (k == numbers.get(y) + numbers.get(x)) {
                 System.out.println(true);
+                break;
             }
         }
     }
