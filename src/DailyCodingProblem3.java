6851612d38e4ab2caac365dc233d63df8b89cb9b
Jacek Pietraszek <jacekpietraszek01@gmail.com>
2024-02-16 21:26:22 +0100

    Solves problem #3

---
+++ src/DailyCodingProblem3.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class DailyCodingProblem3 {
+
+    public static void main(String[] args) {
+
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<Integer> numbers2 = new ArrayList<>();
+
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(8);
+        numbers.add(1);
+
+        System.out.println(numbers);
+
+        for (int x = 1; x < 100; x++) {
+            for (int y = 0; y < numbers.size(); y++) {
+                if (numbers.get(y) == x) {
+                    numbers2.add(x);
+                }
+            }
+        }
+
+        System.out.println(numbers2);
+
+
+        for (int y = 0; y < numbers.size(); y++) {
+            if (numbers2.get(y) != y+1) {
+                System.out.println("Brakująca liczba to: " + (y+1));
+                break;
+            }
+        }
+    }
+}
